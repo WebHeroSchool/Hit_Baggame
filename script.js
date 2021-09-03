1339830710843dff163bc5374587fca811fd531b
YuliyaHit <nata.lychko@mail.ru>
2021-09-03 15:53:05 +0300

    Сделала адаптив по 10 картам

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let level = document.querySelectorAll('.level-item'),
 	startpage = document.querySelector('.start-page'),
 	button = document.querySelector('.start-page__button'),
-	card = document.querySelectorAll('.card'),
 	game__field = document.querySelector('.game__field');
 	
 //  Выбор уровня
@@ -24,6 +23,7 @@ let counterClick=0;
 
 const flipCard = (event) => {
 	event.target.className = 'flip-card-game_over';
+	event.target.classList.add('rotate');
 	counterClick++;
 	if (counterClick > 1) window.location.reload();
 }
@@ -36,32 +36,39 @@ button.addEventListener('click', function() {
 for (let i = 0; i < level.length; i++) {
 	if (level[i].classList.contains('active')) {	
 		let counterCards = level[i].getAttribute('data-card');				
-			const windowInnerWidth = document.documentElement.clientWidth;
-			
+			const windowInnerWidth = window.innerWidth;
 			for (let i = 0; i < counterCards; i++) {
 				createCard();
-			if (counterCards == 6 || counterCards ==3) {
-				game__field.style.maxWidth = 960 + 'px';
-			} 
-
-			if (counterCards == 10) {
-				game__field.style.maxWidth = 1385 + 'px';
-				if ( windowInnerWidth <= 1408 &&  windowInnerWidth >= 1114) {
-					card.forEach(item, () => {
-						item.style.width = 200 + 'px';
-						item.style.width = 312 + 'px';
-					})
-				};
-
-				if ( windowInnerWidth <= 950 &&  windowInnerWidth >= 769) {
-					card.forEach(item, () => {
-						item.style.width = 160 + 'px';
-						item.style.width = 246 + 'px';
-					})
-				};
+				
 
 			}
-		}
+
+				if (counterCards == 6 || counterCards ==3) {
+					game__field.style.maxWidth = 960 + 'px';
+				} 
+
+				if (counterCards == 10) {
+					game__field.style.maxWidth = 1385 + 'px';
+					
+					// if ( windowInnerWidth <= 1408 &&  windowInnerWidth >= 1114) {
+					// 	document.querySelectorAll('.card').forEach(function(item) {
+					// 	item.style.width = 200 + 'px';
+					// 	item.style.height = 312 + 'px';
+					// 	});
+					// } else if ( windowInnerWidth <= 950 &&  windowInnerWidth >= 769) {
+					// 	document.querySelectorAll('.card').forEach(function(item) {
+					// 	item.style.width = 160 + 'px';
+					// 	item.style.height = 246 + 'px';
+					// 	});
+					// };
+
+					cards10 = document.querySelectorAll('.card');
+					console.log(cards10);
+					cards10.forEach(function(item) {
+						item.classList.add('cards10');
+						item.classList.remove('card');
+					});
+				}
 
 		let bag = Math.floor(Math.random()*counterCards);
 		let cardBag  = document.querySelectorAll('.card')[bag];
