5cd47ace4ea6a8b92e8fa79c915f8e520c95b8fa
YuliyaHit <nata.lychko@mail.ru>
2021-09-03 11:53:41 +0300

    ytn ckjd

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let level = document.querySelectorAll('.level-item'),
 	startpage = document.querySelector('.start-page'),
 	button = document.querySelector('.start-page__button'),
+	card = document.querySelectorAll('.card'),
 	game__field = document.querySelector('.game__field');
 	
 //  Выбор уровня
@@ -35,12 +36,31 @@ button.addEventListener('click', function() {
 for (let i = 0; i < level.length; i++) {
 	if (level[i].classList.contains('active')) {	
 		let counterCards = level[i].getAttribute('data-card');				
+			const windowInnerWidth = document.documentElement.clientWidth;
+			
 			for (let i = 0; i < counterCards; i++) {
 				createCard();
 			if (counterCards == 6 || counterCards ==3) {
 				game__field.style.maxWidth = 960 + 'px';
-			} else if (counterCards == 10) {
+			} 
+
+			if (counterCards == 10) {
 				game__field.style.maxWidth = 1385 + 'px';
+				if ( windowInnerWidth <= 1408 &&  windowInnerWidth >= 1114) {
+					card.forEach(item, () => {
+						item.style.width = 200 + 'px';
+						item.style.width = 312 + 'px';
+					})
+				};
+
+				if ( windowInnerWidth <= 950 &&  windowInnerWidth >= 769) {
+					card.forEach(item, () => {
+						item.style.width = 160 + 'px';
+						item.style.width = 246 + 'px';
+					})
+				};
+
+
 			}
 		}
 
